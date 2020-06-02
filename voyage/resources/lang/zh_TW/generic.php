a5bed04b6589122af2f56c2ddfb564c04cd8b55b
Cherry Chung <bubibuuu@gmail.com>
2020-06-02 09:02:17 +0800

    laracast\flash, shopping cart

---
+++ voyage/resources/lang/zh_TW/generic.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    "welcome" => "歡迎使用我們的網頁服務",
+    "username" => "帳號",
+    "password" => "密碼",
+    "login" => "登入",
+    "register" => "註冊",
+    "remember" => "記住密碼",
+    "forget_password" => "忘記密碼"
+];
